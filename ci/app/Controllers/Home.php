240636c4467849b6399b44648528cfe9cf5b9506
elizaveta.falko <lizafloyd2@gmail.com>
2026-01-29 21:33:01 +0700

    fix: mount React to correct root element && added other structure updates && added correct frontend building way

--- ci/app/Controllers/Home.php
+++ ci/app/Controllers/Home.php
@@ -2,15 +2,28 @@
 
 namespace App\Controllers;
 
+// class Home extends BaseController
+// {
+//     public function index()
+//     {
+//         $items = [
+//             'apiUrl' => '/api/items'
+//         ];
+
+//         return view('home', $items);
+//     }
+// }
+
+
 class Home extends BaseController
 {
     public function index()
     {
-        $items = [
-            'apiUrl' => '/api/items'
-        ];
-
-        return view('home', $items);
+        return $this->response
+            ->setHeader('Content-Type', 'text/html')
+            ->setBody(
+                file_get_contents(FCPATH . 'assets/frontend/index.html')
+            );
     }
 }
 
--- ci/app/Views/layout.php
+++ ci/app/Views/layout.php
@@ -15,5 +15,6 @@
 <?= $this->renderSection('content') ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="/assets/frontend/assets/index.js"></script>
 </body>
 </html>
